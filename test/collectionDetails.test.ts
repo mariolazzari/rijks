be6bf54d347af401727abd03c8a1babc80fa3f65
Mario Lazzari <mario.lazzari@gmail.com>
2023-10-21 18:52:43 +0200

    feat: ArtObjectPage

--- test/collectionDetails.test.ts
+++ test/collectionDetails.test.ts
@@ -1,14 +1,23 @@
 import { describe, it, expect } from "vitest";
 import { rijks } from "./global";
 
+const objectNumber = "SK-C-5";
+
 describe("Collection Details API", async () => {
   const res = await rijks.getCollectionDetails({
-    objectNumber: "SK-C-5",
-    format: "json",
+    objectNumber,
   });
 
   it("should return 'The night watch' details", () => {
+    // success state
     expect(res.success).toBeTruthy();
+    expect(res.status).toEqual(200);
+
+    // collection details
+    expect(res.data?.artObjectPage.objectNumber).toBe(objectNumber);
+    expect(res.data?.artObjectPage.plaqueDescription).toBeDefined();
+
+    // no errors
     expect(res.error).toBeUndefined();
   });
 });
